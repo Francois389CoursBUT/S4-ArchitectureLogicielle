76eecc580c8c2678e481f00bc0259d18eeedcf22
François SP <francois-sp@gmx.fr>
2024-03-13 13:57:17 +0100

    TP6

---
+++ TP6-IntroductionJEE/src/main/java/org/fsp/tp6introductionjee/HelloServlet.java
@@ -0,0 +1,28 @@
+package org.fsp.tp6introductionjee;
+
+import java.io.*;
+
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = " World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<B>Bonjour " + request.getParameter("Nom") + "</B>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
---
+++ TP6-IntroductionJEE/src/main/webapp/index.jsp
@@ -0,0 +1,14 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>JSP - Hello World</title>
+    </head>
+    <body>
+    <h1>Entrez votre nom</h1>
+    <form action="hello-servlet">
+        nom <input type="text" name="Nom" value="" size="30"/>
+        <input type="submit" value="Ok"/>
+    </form>
+    </body>
+</html>
